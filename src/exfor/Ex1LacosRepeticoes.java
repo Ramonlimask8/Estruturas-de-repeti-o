086e97744445fc7050e9bdc00e798bb97cfe84a5
Ramon Ferreira <ramonlima0912@gmail.com>
2017-04-10 21:56:54 -0300

    Numeros Pares de 1 a 100 Usando o While

--- src/exfor/Ex1LacosRepeticoes.java
+++ src/exfor/Ex1LacosRepeticoes.java
@@ -5,14 +5,17 @@ package exfor;
 public class Ex1LacosRepeticoes {
 
     public static void main(String[] args) {
-     
-        int i = 100;
-        while(i > 1){
-System.out.println("Linha " + i);
-        i = i - 2 ;
-    }
-
-    }
+    int i = 100;
+     while (0 < i){
+         System.out.println("Linha " + i);
+         i = i - 2;
+         
+     }
     
 }
+}
+
+
+    
+
 
